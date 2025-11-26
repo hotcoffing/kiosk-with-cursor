730ef4cb94579ad87c9cba5344f221f5584b91b3
KimSungBin <skuy369@hansung.ac.kr>
2025-11-27 02:33:45 +0900

    컨트롤러 확립 및 의존성 주입 완료 - 11/23

---
+++ Kiosk-final-project/src/main/java/SwingComponent/SelectOptionFrame.java
@@ -0,0 +1,13 @@
+package SwingComponent;
+
+public class SelectOptionFrame {
+    private final SwingGraphic swingGraphic;
+    private final SwingAction  swingAction;
+    private final SwingController swingController;
+
+    public SelectOptionFrame(SwingGraphic swingGraphic, SwingAction swingAction, SwingController swingController) {
+        this.swingGraphic = swingGraphic;
+        this.swingAction = swingAction;
+        this.swingController = swingController;
+    }
+}
---
+++ Kiosk-final-project/src/main/java/SwingComponent/ShoppingCartFrame.java
@@ -0,0 +1,11 @@
+package SwingComponent;
+
+public class ShoppingCartNewTabFrame {
+    private final SwingGraphic swingGraphic;
+    private final SwingController swingController;
+
+    public ShoppingCartNewTabFrame(SwingGraphic swingGraphic, SwingController swingController) {
+        this.swingGraphic = swingGraphic;
+        this.swingController = swingController;
+    }
+}
